48c42878f463ce8ff8c582a5d078ccdce4c37410
John Raesly <johnraesly@microsoft.com>
2023-02-07 12:16:00 -0700

    My first action is ready

---
+++ index.js
@@ -0,0 +1,50 @@
+const axios = require("axios");
+const core = require('@actions/core');
+
+async function updateFlowOwners(bearerToken, orgUrl, ownerId) {
+    const flows = await axios.get(`${orgUrl}/api/data/v9.1/workflows`, {
+        headers: {
+            Authorization: `Bearer ${bearerToken}`,
+        },
+    });
+    console.log(`Flows Retrieved. Count: ${flows.Count}`);
+    flows.data.forEach(async flow => {
+        const ownerId = `systemusers(${ownerId})`;
+
+        await axios.patch(`${orgUrl}/api/data/v9.1/workflows(${flow.workflowid})`, {
+            ownerid: ownerId,
+        }, {
+            headers: {
+                Authorization: `Bearer ${bearerToken}`,
+                'OData-Version': '4.0',
+                'Content-Type': 'application/json',
+                'OData-MaxVersion': '4.0',
+                'OData-EntityId': `workflows(${flow.workflowid})`,
+            },
+        });
+    });
+}
+
+function main(clientId, clientSecret, tenantId, orgUrl, environmentId) {
+    console.log("Entering main...")
+    const clientId = core.getInput('clientId', { required: true });
+    const clientSecret = core.getInput('clientSecret', { required: true });
+    const tenantId = core.getInput('tenantId', { required: true });
+    const orgUrl = core.getInput('orgUrl', { required: true });
+    const environmentId = core.getInput('environmentId', { required: true });
+    const ownerId = core.getInput('ownerId', { required: true });
+    console.log("Grabbed Variables Successfully");
+    try {
+        const bearerToken = await generateBearerToken(clientId, clientSecret, tenantId, environmentId);
+        updateFlowOwners(bearerToken, orgUrl, ownerId);
+        console.log(`Flows Updated Successfully`);
+        console.log("Exiting....")
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+// Call the main function to run the action
+main();
+
