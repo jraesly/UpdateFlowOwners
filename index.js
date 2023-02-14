f53a5de308c063c367139f6d666241cfd5562ea2
John Raesly <johnraesly@microsoft.com>
2023-02-13 18:19:58 -0700

    Updates

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const axios = require("axios");
 const core = require('@actions/core');
 
-async function generateBearerToken(clientId, clientSecret, tenantId, environmentId, orgUrl) {
-    const response = await axios.post(`https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`,
-        `client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials&scope=https://api.crm.dynamics.com/${environmentId}`);
-    console.log("Bearer token Acquired");
-    return response.data.access_token;
-}
 async function getBearerToken(clientId, clientSecret, tenantId, orgUrl) {
     try {
         const response = await axios.post(
@@ -24,8 +18,8 @@ async function getBearerToken(clientId, clientSecret, tenantId, orgUrl) {
         console.error(error);
     }
 }
-async function updateFlowOwners(bearerToken, orgUrl, ownerId) {
-    const flows = await axios.get(`${orgUrl}/api/data/v9.1/workflows?$filter=category eq 5 and category eq 5 and _ownerid_value ne ${ownerId}`, {
+async function updateFlowOwners(bearerToken, orgUrl, ownerId, category) {
+    const flows = await axios.get(`${orgUrl}/api/data/v9.1/workflows?$filter=category eq ${category} and _ownerid_value ne ${ownerId}`, {
         headers: {
             Authorization: `Bearer ${bearerToken}`,
         },
@@ -40,8 +34,8 @@ async function updateFlowOwners(bearerToken, orgUrl, ownerId) {
         console.log(`updating flow now: ${count}`);
         axios.patch(`${orgUrl}/api/data/v9.1/workflows(${flow.workflowid})`,
             {
-                "description": "This flow will ensure consistency across systems.",
-                "statecode": 1,
+ //               "description": "This flow will ensure consistency across systems.",
+//                "statecode": 1,
                 "ownerid@odata.bind": `systemusers(${ownerId})`
             },
             {
@@ -75,20 +69,20 @@ async function updateFlowOwners(bearerToken, orgUrl, ownerId) {
     console.log("Exiting....")
 }
 
-async function main(/*clientId, clientSecret, tenantId, orgUrl, environmentId*/) {
+async function main() {
     console.log("Entering main...")
-    clientId = //core.getInput('clientId', { required: true });
-    clientSecret = //core.getInput('clientSecret', { required: true });
-    tenantId =  //core.getInput('tenantId', { required: true });
-    orgUrl = //core.getInput('orgUrl', { required: true });
-    environmentId = //INT"8098603c-68a4-47f5-ba13-6ddb840b8589";//"0bc156d3-bd12-4b08-a73c-cdb5b7a81a28"; build //core.getInput('environmentId', { required: true });
-    ownerId = //core.getInput('ownerId', { required: true });
+    var clientId = core.getInput('clientId', { required: true });
+    var clientSecret = core.getInput('clientSecret', { required: true });
+    var tenantId =  core.getInput('tenantId', { required: true });
+    var orgUrl = core.getInput('orgUrl', { required: true });
+    var ownerId = core.getInput('ownerId', { required: true });
+    var category = core.getInput('category', { required: true });
 
     console.log("Grabbed Variables Successfully");
     try {
         const bearerToken = await getBearerToken(clientId, clientSecret, tenantId, orgUrl);
         console.log(`Entering Flow Updates`);
-        updateFlowOwners(bearerToken, orgUrl, ownerId);
+        updateFlowOwners(bearerToken, orgUrl, ownerId, category);
     }
     catch (error) {
         console.log(error.message);
