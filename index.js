8440d378a6db35ea33ca2abd3889dd505619bba5
John Raesly <johnraesly@microsoft.com>
2023-03-08 21:43:25 -0700

    Added Retry function and query url so system is not required

--- index.js
+++ index.js
@@ -22,14 +22,26 @@ async function getBearerToken(clientId, clientSecret, tenantId, orgUrl) {
     }
 }
 
+
 async function updateFlowOwners(bearerToken, orgUrl, ownerId, system) {
+    let flows = null;
     // Retrieve all modern flows that do not already have the desired owner and filters out 'SYSTEM' Account flows
-    const flows = await axios.get(`${orgUrl}/api/data/v9.1/workflows?$filter=category eq 5 and _ownerid_value ne ${ownerId} and _ownerid_value ne ${system}`, {
-        headers: {
-            Authorization: `Bearer ${bearerToken}`,
-        },
-    });
-    console.log(`Flows Retrieved. Count: ${flows.data.value.length}`);
+    try {
+        flows = await axios.get(QueryUrl(orgUrl, ownerId, system), {
+            headers: {
+                Authorization: `Bearer ${bearerToken}`,
+            },
+        });
+        console.log(`Flows Retrieved. Count: ${flows.data.value.length}`);
+    } catch (error) {
+        console.error(error);
+        console.log(`Retrying...`);
+        await retry(axios.get, [QueryUrl(orgUrl, ownerId, system), {
+            headers: {
+                Authorization: `Bearer ${bearerToken}`,
+            },
+        }]);
+    }
 
     async function updateFlow(flow) {
         console.log(`updating flow now: ${i}`);
@@ -50,27 +62,45 @@ async function updateFlowOwners(bearerToken, orgUrl, ownerId, system) {
                     }
                 }
             );
-        } 
-        catch (error) 
-        {
-            // this is the main part. Use the response property from the error object
-            console.log(flow.name + ': ' + error.response.data.error.message); 
-            console.log('Please update ' + flow.name + ' connection references as this is the usual culprit')
+        } catch (error) {
+            console.log(flow.name + ': ' + error.response.data.error.message);
+            console.log('Please update ' + flow.name + ' connection references as this is the usual culprit');
         }
-        // increment counter for logging purposes
         i++;
-        if (i < flows.data.value.length) 
-        {
+        if (i < flows.data.value.length) {
             console.log('Waiting one second to avoid 429: Too Many Requests Error')
             setTimeout(() => updateFlow(flows.data.value[i]), 1000);
-        } else 
-        {
+        } else {
             console.log(`Flows Updated Successfully`);
             console.log("Exiting....");
         }
     }
     updateFlow(flows.data.value[i]);
 }
+
+async function retry(func, args) {
+    const maxRetries = 3;
+    let retries = 0;
+    while (retries < maxRetries) {
+        try {
+            const response = await func(...args);
+            return response;
+        } catch (error) {
+            console.error(error);
+            retries++;
+            console.log(`Retrying in ${retries} second(s)`);
+            await new Promise(resolve => setTimeout(resolve, retries * 1000));
+        }
+    }
+    console.log(`Max retries reached. Exiting...`);
+    process.exit(1);
+}
+
+
+function QueryUrl(orgUrl, ownerId, system) {
+    return system != null ? `${orgUrl}/api/data/v9.1/workflows?$filter=category eq 5 and _ownerid_value ne ${ownerId} and _ownerid_value ne ${system}` : `${orgUrl}/api/data/v9.1/workflows?$filter=category eq 5 and _ownerid_value ne ${ownerId}`;
+}
+
 
 
 async function main() {
@@ -79,9 +109,9 @@ async function main() {
     var clientId =  core.getInput('clientId', { required: true });
     var clientSecret = core.getInput('clientSecret', { required: true });
     var tenantId = core.getInput('tenantId', { required: true });
-    var orgUrl = core.getInput('orgUrl', { required: true });
+    var orgUrl =  core.getInput('orgUrl', { required: true });
     var ownerId = core.getInput('ownerId', { required: true });
-    var system = core.getInput('system', { required: true });
+    var system =  core.getInput('system', { required: true });
 
     console.log("Grabbed Variables Successfully");
     try {
